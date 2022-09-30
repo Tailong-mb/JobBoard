3f9bb5f617dfbee681460661168a68dbb2a16466
Michael Bardou <michael.bardou@icloud.com>
2022-09-30 10:30:01 +0200

    global.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   module: ["@nuxt/ui"],
-  css: ["@/assets/css/fonts.css"],
+  css: ["@/assets/css/fonts.css", "@/assets/css/global.css"],
   publicRuntimeConfig: {
     SUPABASE_KEY: process.env.SUPABASE_KEY,
     SUPABASE_URL: process.env.SUPABASE_URL,
