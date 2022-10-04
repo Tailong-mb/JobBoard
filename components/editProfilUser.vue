97c4ea7dfcb7dbff26f69285c263ace9b57fbcca
francois <langfrancoisfrederic@gmail.com>
2022-10-04 17:17:08 +0200

    animation home + animation profil user and change data

---
+++ components/editProfilUser.vue
@@ -0,0 +1,173 @@
+<script setup lang="ts">
+
+import {gsap} from "gsap";
+
+const returnButton = () => {
+
+    const timeline = gsap.timeline({default: {duration: 1}})
+    timeline.to('.container', {opacity: "0", ease: 'power1'});
+
+}
+
+</script>
+
+
+<template>
+
+    <div class="container">
+        <div
+        class="sign-in"
+        >
+        <div class="subTitle">Profil</div>
+        <div class="sign-in-form">
+            <div class="sign-in-form-data">
+                <div class="input-data">
+                    <input
+                    type="text"
+                    name="email"
+                    class="text"
+                    required
+                    />
+                    <div class="underline"></div>
+                    <label class="text">Email</label>
+                </div>
+                <div class="input-data">
+                    <input
+                    class="text"
+                    type="text"
+                    name="password"
+                    required
+                    />
+                    <div class="underline"></div>
+                    <label class="text">phone number</label>
+                </div>
+                <div class="input-data">
+                    <input
+                    class="text"
+                    type="text"
+                    name="password"
+                    required
+                    />
+                    <div class="underline"></div>
+                    <label class="text">first name</label>
+                </div>
+                <div class="input-data">
+                    <input
+                    class="text"
+                    type="text"
+                    name="password"
+                    required
+                    />
+                    <div class="underline"></div>
+                    <label class="text">last name</label>
+                </div>
+                <div class="input-data">
+                    <input
+                    class="text"
+                    type="text"
+                    name="password"
+                    required
+                    />
+                    <div class="underline"></div>
+                    <label class="text">grade</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="confirm">
+            <rectangle-button text="confirm" @click="returnButton"></rectangle-button>
+        </div>
+    </div>
+    </div>
+</template>
+
+
+
+<style scoped>
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    gap: 3rem;
+}
+
+.sign-in {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    margin-top: 2rem;
+}
+
+.sign-in-form {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+    align-items: center;
+}
+
+.sign-in-form-data {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+
+.subTitle{
+    top: 5rem;
+}
+
+.input-data {
+    height: 2.5rem;
+    width: 100%;
+    position: relative;
+}
+
+input {
+    height: 100%;
+    width: 100%;
+    border: none !important;
+    border-bottom: 2px solid silver;
+}
+
+input[type="text"],
+input[type="email"] {
+    background: transparent;
+    border: none !important;
+}
+
+input:focus ~ label,
+input:valid ~ label {
+    transform: translateY(-1.25rem);
+    color: rgb(81, 79, 79);
+}
+
+label {
+    position: absolute;
+    bottom: 0.626rem;
+    left: 0;
+    color: #00454f;
+    pointer-events: none;
+    transition: all 0.3s ease;
+}
+
+.underline {
+    position: absolute;
+    bottom: 0;
+    height: 0.125rem;
+    width: 100%;
+    background: #8f71be;
+}
+
+.confirm{
+    bottom: 7rem;
+}
+
+.password-forgotten{
+    margin-top: 2rem;
+    cursor: pointer;
+}
+
+
+</style>
---
+++ pages/signIn/homeUser.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+    import {gsap} from "gsap";
+
+    const auth = useAuth();
+    const { supabase } = useSupabase();
+    
+    const signUpUser = reactive({
+        email: "",
+        password: "",
+        name: "",
+        surname: "",
+        location: "",
+        description: "",
+    });
+
+    const editButton = () => {
+        gsap.to('.container', {opacity: "0"})
+        gsap.to('.edit-profil',{display: "flex", opacity: 1, delay: 1})
+    }
+
+
+
+    
+    </script>
+    
+    <template>
+
+        <logo></logo>
+        <div class="container">
+            <div class="container-info">
+                <div class="header">
+                    <div class="subTitle">grade</div>
+                    <div class="informations">
+                        <div class="text">mail</div>
+                        <div class="text">phone number</div>
+                    </div>
+                    
+                </div>
+                <h2>Name</h2>
+                <text class="text">Description text</text>
+            </div>
+            <rectangle-button class="button" text="edit profil" @click="editButton"></rectangle-button>
+        </div>
+
+        <edit-profil-user class="edit-profil" ></edit-profil-user>
+            
+    </template>
+    
+    <style scoped>
+    .container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        width: 100%;
+        top: 5rem;
+        position: relative;
+        gap: 3rem;
+    }
+    
+    .subTitle{
+        padding-right: 1rem;
+    }
+    .header{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+    }
+
+    .container-info{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        gap: 1rem;
+    }
+    .button{
+        position: absolute;
+        bottom: -15rem;
+    }
+
+    .edit-profil{
+        display: none;
+        position: absolute;
+        top: 2rem;
+    }
+
+    </style>
