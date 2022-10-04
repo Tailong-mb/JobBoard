cf306e6c59f7fa188f4b96b1a786bdc4d32b140f
Michael Bardou <michael.bardou@icloud.com>
2022-10-04 13:51:58 +0200

    cardCandidate

--- pages/candidacies/index.vue
+++ pages/candidacies/index.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <Jobcard
-    company-name="Episteak"
-    date="2018-2019"
-    job-location="Nancy"
-    degreeRequired="Master"
-    jobTitle="testetsgag"
-    jobDescription="lorem alzvndzomvbzvbzekVBZbvkzbvkdsbvksbskvbskvbskvbskvbskvsbvskjbvksv sk skjvueibzvb"
-    salary="5000"
-  ></Jobcard>
+  <CandidateCard
+    name="Tom"
+    degree="Degree"
+    email="michael.bardou@outlook.fr"
+  ></CandidateCard>
 </template>
 
 <style scoped></style>
