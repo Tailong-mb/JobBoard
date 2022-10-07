f7f38b6211bf7e0f56c117d7cf02de7eb56ac457
Michael Bardou <michael.bardou@icloud.com>
2022-10-07 10:37:04 +0200

    Prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,207 +1,195 @@
 <!-- home page -->
 
 <script setup lang="ts">
-import Logo1 from '~~/components/logo.vue';
 const { supabase } = useSupabase();
 </script>
 
 <template>
+  <animation-load-home> </animation-load-home>
 
-    <animation-load-home>
+  <div class="container">
+    <div class="logo">
+      <logo></logo>
+    </div>
 
-    </animation-load-home>
-
-    <div class="container">
-        <div class="logo">
-            <logo></logo>
-        </div>
-
-        <div class="subscribe">
-            <div class="signIn">
-                <CircleButton text="Sign In"></CircleButton>
-            </div>
-            <div class="signUp">
-                <CircleButton text="Sign Up"></CircleButton>
-            </div>
-        </div>
-
-        <div class="bigTitle">
-            <div class="wordNear">
-                NEAR
-            </div>
-            <div class="wordYou">
-                YOU
-            </div>
-        </div>
-
-        <div class="text">
-            <span class="nearYou">Near <span class="you">You</span></span> is the first platform in numerics allowing meetings between companies and encoders.<br />
-            Average hiring rate lower than three weeks and average salary is equal to 50k$ per year.<br />
-            In partnership with <span class="epi">Epi<span class="steak">Steak</span></span>.
-        </div>
+    <div class="subscribe">
+      <div class="signIn">
+        <CircleButton text="Sign In"></CircleButton>
+      </div>
+      <div class="signUp">
+        <CircleButton text="Sign Up"></CircleButton>
+      </div>
     </div>
 
     <div class="bigTitle">
       <div class="wordNear">NEAR</div>
       <div class="wordYou">YOU</div>
     </div>
+
+    <div class="text">
+      <span class="nearYou">Near <span class="you">You</span></span> is the
+      first platform in numerics allowing meetings between companies and
+      encoders.<br />
+      Average hiring rate lower than three weeks and average salary is equal to
+      50k$ per year.<br />
+      In partnership with
+      <span class="epi">Epi<span class="steak">Steak</span></span
+      >.
+    </div>
+  </div>
+
+  <div class="bigTitle">
+    <div class="wordNear">NEAR</div>
+    <div class="wordYou">YOU</div>
   </div>
 </template>
 
 <style scoped>
-    .container{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        position: relative;
-        opacity: 0;
-        animation: vanish 2s 3.5s ease-in-out forwards;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  opacity: 0;
+  animation: vanish 2s 3.5s ease-in-out forwards;
+}
 
-    @keyframes vanish{
-        0%{
-            opacity: 0;
-        }
-        100%{
-            opacity: 1;
-        }
-    }
-    .subscribe{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        bottom: -15rem;
-        color: #00454f;
-    }
+@keyframes vanish {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.subscribe {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  bottom: -15rem;
+  color: #00454f;
+}
 
-    .bigTitle{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        position: absolute;
-        bottom:-35rem;
-    }
+.bigTitle {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  position: absolute;
+  bottom: -35rem;
+}
 
 .wordNear {
   color: #00454f;
 }
 
-    .wordYou{
-        color: #8F71BE;
-    }
-
-    .text{
-        display: none;
-    }
-
-    .nearYou{
-        color: #00454f;
-        font-style: italic;
-    }
-
-    .you{
-        color: #8F71BE;
-    }
-    .steak{
-        color: #8F71BE;
-        font-style: italic;
-    }
-
-@media screen and (min-width: 600px) {
-
-    .container{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        position: relative;
-    }
-
-    .logo{
-        position: absolute;
-        right: 5rem;
-    }
-    
-    .bigTitle{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        position: absolute;
-        left: 5rem;
-    }
-
-    .subscribe{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        bottom: -42rem;
-        color: #00454f;
-    }
-
-    .text{
-        display: block;
-        position: absolute;
-        right: 5rem;
-        top: 20rem;
-        width: 20rem;
-    }
-
+.wordYou {
+  color: #8f71be;
 }
 
+.text {
+  display: none;
+}
 
-    
+.nearYou {
+  color: #00454f;
+  font-style: italic;
+}
+
+.you {
+  color: #8f71be;
+}
+.steak {
+  color: #8f71be;
+  font-style: italic;
+}
+
+@media screen and (min-width: 600px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+
+  .logo {
+    position: absolute;
+    right: 5rem;
+  }
+
+  .bigTitle {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    position: absolute;
+    left: 5rem;
+  }
+
+  .subscribe {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    bottom: -42rem;
+    color: #00454f;
+  }
+
+  .text {
+    display: block;
+    position: absolute;
+    right: 5rem;
+    top: 20rem;
+    width: 20rem;
+  }
+}
+
 @media screen and (min-width: 1900px) {
+  .logo {
+    position: absolute;
+    right: 10rem;
+  }
+  .text {
+    display: block;
+    position: absolute;
+    right: 5rem;
+    top: 20rem;
+    width: 40rem;
+  }
 
-    .logo{
-        position: absolute;
-        right: 10rem;
-    }
-    .text{
-        display: block;
-        position: absolute;
-        right: 5rem;
-        top: 20rem;
-        width: 40rem;
-    }
+  .bigTitle {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    position: absolute;
+    left: 10rem;
+    top: 15rem;
+  }
 
-    .bigTitle{
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        position: absolute;
-        left: 10rem;
-        top: 15rem;
-    }
-
-    .subscribe{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-items: center;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        bottom: -60rem;
-        color: #00454f;
-    }
-}   
-
-    
-
+  .subscribe {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    bottom: -60rem;
+    color: #00454f;
+  }
+}
 </style>
