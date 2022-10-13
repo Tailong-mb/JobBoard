2fd08ce318b2e595b9ef4a89cd0e29db2bf9618e
Michael Bardou <michael.bardou@icloud.com>
2022-10-13 10:04:31 +0200

    useDBCandidacy

---
+++ composables/useDBCandidacy.ts
@@ -0,0 +1,67 @@
+const useDBCandidacy = () => {
+  const { supabase } = useSupabase();
+
+  const getAllcandidacy = async () => {
+    const { data, error } = await supabase.from("candidacies").select("*");
+    if (error) throw error;
+    return data;
+  };
+
+  const getCandidacyByJobId = async (id) => {
+    const { data, error } = await supabase
+      .from("candidacies")
+      .select("*")
+      .eq("id_job", id);
+    if (error) throw error;
+    return data;
+  };
+
+  const getCandidacyByUserId = async (id) => {
+    const { data, error } = await supabase
+      .from("candidacies")
+      .select("*")
+      .eq("id_worker", id);
+    if (error) throw error;
+    return data;
+  };
+
+  const insertCandidacy = async (candidacy) => {
+    const { error } = await supabase.from("candidacies").insert([
+      {
+        id_worker: candidacy.id_worker,
+        id_job: candidacy.id_job,
+        status: candidacy.status,
+      },
+    ]);
+    if (error) throw error;
+  };
+
+  const updateStatusCandidacy = async (candidacy) => {
+    const { error } = await supabase
+      .from("candidacies")
+      .update({ status: candidacy.status })
+      .eq("id_worker", candidacy.id_worker)
+      .eq("id_job", candidacy.id_job);
+    if (error) throw error;
+  };
+
+  const deleteCandidacy = async (candidacy) => {
+    const { error } = await supabase
+      .from("candidacies")
+      .delete()
+      .eq("id_worker", candidacy.id_worker)
+      .eq("id_job", candidacy.id_job);
+    if (error) throw error;
+  };
+
+  return {
+    getAllcandidacy,
+    getCandidacyByJobId,
+    getCandidacyByUserId,
+    insertCandidacy,
+    updateStatusCandidacy,
+    deleteCandidacy,
+  };
+};
+
+export default useDBCandidacy;
